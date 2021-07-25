de6002188eccb012cf252527f50caf80ca88140d
takeumi.ishihara <takeumiishihara@gmail.com>
2021-07-25 04:11:45 +0000

    app

--- app/User.php
+++ app/User.php
@@ -42,7 +42,7 @@ class User extends Authenticatable
      */
      public function microposts()
      {
-         return $this->hasMany(Microposts::class);
+         return $this->hasMany(Micropost::class);
      }
     
     /**
