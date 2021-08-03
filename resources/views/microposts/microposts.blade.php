2fed60868c1969bec1eedf46c3cb1afac1b4d617
takeumi.ishihara <takeumiishihara@gmail.com>
2021-08-03 06:25:54 +0000

    blade

--- resources/views/microposts/microposts.blade.php
+++ resources/views/microposts/microposts.blade.php
@@ -15,12 +15,12 @@
                         <p class="mb-0">{!! nl2br(e($micropost->content)) !!}</p>
                     </div>
                     <div>
-                    @if (Auth::id() ==$micropost->user_id)
-                    {{--投稿削除ボタンのフォーム --}}
-                    {!! Form::open(['route' =>['microposts.destroy',$micropost->id],'method' => 'delete']) !!}
-                    {!! Form::submit('Delete',['class' =>'btn btn-danger btn-sm']) !!}
-                    {!! sForm::close() !!}
-                    @endif
+                        @if (Auth::id() ==$micropost->user_id)
+                        {{--投稿削除ボタンのフォーム --}}
+                        {!! Form::open(['route' =>['microposts.destroy',$micropost->id],'method' => 'delete']) !!}
+                        {!! Form::submit('Delete',['class' =>'btn btn-danger btn-sm']) !!}
+                        {!! sForm::close() !!}
+                        @endif
                     </div>
                 </div>
             </li>
